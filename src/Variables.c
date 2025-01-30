809c3069fd6a3ccb002942cd12af22f5144caa6a
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-30 23:42:52 +0600

    Create Variables.c

---
+++ src/Variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    float salary = 50000.50;
+    char grade = 'A';
+    
+    printf("Age: %d\n", age);
+    printf("Salary: %.2f\n", salary);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
