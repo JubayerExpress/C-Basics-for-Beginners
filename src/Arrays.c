d9519af5c68eaffcaaf595cb23bbed14d752ebc4
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-30 23:44:31 +0600

    Create Arrays.c

---
+++ src/Arrays.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5};
+    
+    for (int i = 0; i < 5; i++) {
+        printf("Number: %d\n", numbers[i]);
+    }
+    
+    return 0;
+}
